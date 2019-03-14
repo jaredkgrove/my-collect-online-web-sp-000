18164844917cc2c7891cb8c5030d98bf6b59d4aa
Jared Grove <jaredkgrove@gmail.com>
2019-03-14 22:00:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   index = 0 
   new_collection = []
   while index < collection.size 
-    new_collection << yield(collection[i])
+    new_collection << yield(collection[index])
     index += 1
   end
   new_collection
